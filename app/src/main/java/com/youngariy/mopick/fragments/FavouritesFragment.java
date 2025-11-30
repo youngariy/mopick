981b7b216ab8f634661da22028a233af9b321735
youngariy <dudqja8617@gmail.com>
2025-11-30 17:23:45 +0900

    tab box

--- app/src/main/java/com/youngariy/mopick/fragments/FavouritesFragment.java
+++ app/src/main/java/com/youngariy/mopick/fragments/FavouritesFragment.java
@@ -7,11 +7,12 @@ import android.view.ViewGroup;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.viewpager.widget.ViewPager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.youngariy.mopick.R;
-import com.google.android.material.tabs.TabLayout;
-import com.youngariy.mopick.adapters.FavouritesPagerAdapter;
+import com.google.android.material.button.MaterialButtonToggleGroup;
+import com.youngariy.mopick.fragments.FavouriteMoviesFragment;
+import com.youngariy.mopick.fragments.FavouriteTVShowsFragment;
 
 /**
  * Created by hitanshu on 10/8/17.
@@ -19,24 +20,43 @@ import com.youngariy.mopick.adapters.FavouritesPagerAdapter;
 
 public class FavouritesFragment extends Fragment {
 
-    private ViewPager mFavViewPager;
-    private TabLayout mFavTabLayout;
-    private FavouritesPagerAdapter mFavPagerAdapter;
+    private MaterialButtonToggleGroup mToggleGroup;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_favourites, container, false);
 
-        mFavViewPager = view.findViewById(R.id.view_pager_fav);
-        mFavTabLayout = view.findViewById(R.id.tab_layout_fav);
+        mToggleGroup = view.findViewById(R.id.toggle_fav);
 
-        mFavPagerAdapter = new FavouritesPagerAdapter(getChildFragmentManager(), getContext());
-        mFavViewPager.setAdapter(mFavPagerAdapter);
-        mFavViewPager.setOffscreenPageLimit(mFavPagerAdapter.getCount() - 1);
-        mFavTabLayout.setupWithViewPager(mFavViewPager);
+        Fragment current = getChildFragmentManager().findFragmentById(R.id.container_fav);
+        if (current == null) {
+            mToggleGroup.check(R.id.btn_fav_movies);
+            showFragment(new FavouriteMoviesFragment());
+        } else if (current instanceof FavouriteTVShowsFragment) {
+            mToggleGroup.check(R.id.btn_fav_tv);
+        } else {
+            mToggleGroup.check(R.id.btn_fav_movies);
+        }
+
+        mToggleGroup.addOnButtonCheckedListener((group, checkedId, isChecked) -> {
+            if (!isChecked) {
+                return;
+            }
+            if (checkedId == R.id.btn_fav_movies) {
+                showFragment(new FavouriteMoviesFragment());
+            } else if (checkedId == R.id.btn_fav_tv) {
+                showFragment(new FavouriteTVShowsFragment());
+            }
+        });
 
         return view;
+    }
+
+    private void showFragment(Fragment fragment) {
+        FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
+        transaction.replace(R.id.container_fav, fragment);
+        transaction.commit();
     }
 }
 
